b843c880d19bb5c01ceb67ed2928cb268f3104fd
Matt Hughes <matt.c.hughes@gmail.com>
2026-02-02 21:26:11 -0700

    Add github and bitbucket ssh host keys

--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@
 SRC=$(readlink -f "${BASH_SOURCE[0]}")
 DIR="$( cd "$( dirname "${SRC}" )" >/dev/null 2>&1 && pwd )"
 
+# Set up git
 ln -s "${DIR}/.gitconfig" ~/.gitconfig
+
+# Install github and bitbucket host keys
+mkdir -p ~/.ssh
+cp "${DIR}/ssh/known_hosts" ~/.ssh/
