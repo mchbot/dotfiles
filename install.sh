1b8c909f05227c7d9f858e56ee864cecee27a569
Matt Hughes <matt.c.hughes@gmail.com>
2026-02-02 22:05:42 -0700

    fix copies

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ SRC=$(readlink -f "${BASH_SOURCE[0]}")
 DIR="$( cd "$( dirname "${SRC}" )" >/dev/null 2>&1 && pwd )"
 
 # Set up git
-ln -s "${DIR}/.gitconfig" ~/.gitconfig
+cp ${DIR}/.gitconfig ~/
 
 # Install github and bitbucket host keys
 mkdir -p ~/.ssh
-cp "${DIR}/.ssh/*" ~/.ssh/
+cp ${DIR}/.ssh/* ~/.ssh/
 chmod 0500 ~/.ssh
 chmod 0400 ~/.ssh/*
 
